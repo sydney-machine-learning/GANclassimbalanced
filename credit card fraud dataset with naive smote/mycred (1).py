4feefff723ee5ad8fe57d1ee8ad961658aacafe8
Prabhat-Kr-Singh <64763235+Prabhat-Kr-Singh@users.noreply.github.com>
2021-01-18 20:38:57 +0530

    Add files via upload

---
+++ credit card fraud dataset with naive smote/mycred (1).py
@@ -0,0 +1,285 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# This Python 3 environment comes with many helpful analytics libraries installed
+# It is defined by the kaggle/python Docker image: https://github.com/kaggle/docker-python
+# For example, here's several helpful packages to load
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+# Input data files are available in the read-only "../input/" directory
+# For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
+
+import os
+for dirname, _, filenames in os.walk('/kaggle/input'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
+
+# You can write up to 20GB to the current directory (/kaggle/working/) that gets preserved as output when you create a version using "Save & Run All" 
+# You can also write temporary files to /kaggle/temp/, but they won't be saved outside of the current session
+
+
+# In[2]:
+
+
+import pandas  as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+# In[3]:
+
+
+data = pd.read_csv('/kaggle/input/creditcardfraud/creditcard.csv')
+data.head(3)
+
+
+# In[4]:
+
+
+pd.value_counts(data['Class']).plot.bar()
+plt.title('Fraud class histogram')
+plt.xlabel('Class')
+plt.ylabel('Frequency')
+data['Class'].value_counts()
+
+
+# In[5]:
+
+
+from sklearn.preprocessing import StandardScaler
+
+data['normAmount'] = StandardScaler().fit_transform(data['Amount'].values.reshape(-1, 1))
+data = data.drop(['Time', 'Amount'], axis=1)
+data.head()
+
+
+# In[6]:
+
+
+X = np.array(data.iloc[:, data.columns != 'Class'])
+y = np.array(data.iloc[:, data.columns == 'Class'])
+print('Shape of X: {}'.format(X.shape))
+print('Shape of y: {}'.format(y.shape))
+
+
+# In[7]:
+
+
+from imblearn.over_sampling import SMOTE
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+
+print("Number transactions X_train dataset: ", X_train.shape)
+print("Number transactions y_train dataset: ", y_train.shape)
+print("Number transactions X_test dataset: ", X_test.shape)
+print("Number transactions y_test dataset: ", y_test.shape)
+
+
+# In[8]:
+
+
+print("Before OverSampling, counts of label '1': {}".format(sum(y_train==1)))
+print("Before OverSampling, counts of label '0': {} \n".format(sum(y_train==0)))
+
+sm = SMOTE(random_state=2)
+X_train_res, y_train_res = sm.fit_sample(X_train, y_train.ravel())
+
+print('After OverSampling, the shape of train_X: {}'.format(X_train_res.shape))
+print('After OverSampling, the shape of train_y: {} \n'.format(y_train_res.shape))
+
+print("After OverSampling, counts of label '1': {}".format(sum(y_train_res==1)))
+print("After OverSampling, counts of label '0': {}".format(sum(y_train_res==0)))
+
+
+# In[9]:
+
+
+from sklearn.model_selection import GridSearchCV
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix, precision_recall_curve, auc, roc_auc_score, roc_curve, recall_score, classification_report
+
+parameters = {
+    'C': np.linspace(1, 10, 10)
+             }
+lr = LogisticRegression()
+clf = GridSearchCV(lr, parameters, cv=5, verbose=5, n_jobs=3)
+clf.fit(X_train_res, y_train_res.ravel())
+
+
+# In[10]:
+
+
+clf.best_params_
+
+
+# In[11]:
+
+
+lr1 = LogisticRegression(C=9,penalty='l2', verbose=5)
+lr1.fit(X_train_res, y_train_res.ravel())
+
+
+# In[12]:
+
+
+import itertools
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=0)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        #print("Normalized confusion matrix")
+    else:
+        1#print('Confusion matrix, without normalization')
+
+    #print(cm)
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+
+# In[13]:
+
+
+y_train_pre = lr1.predict(X_train)
+
+cnf_matrix_tra = confusion_matrix(y_train, y_train_pre)
+
+print("Recall metric in the train dataset: {}%".format(100*cnf_matrix_tra[1,1]/(cnf_matrix_tra[1,0]+cnf_matrix_tra[1,1])))
+
+
+class_names = [0,1]
+plt.figure()
+plot_confusion_matrix(cnf_matrix_tra , classes=class_names, title='Confusion matrix')
+plt.show()
+
+
+# In[14]:
+
+
+y_pre = lr1.predict(X_test)
+
+cnf_matrix = confusion_matrix(y_test, y_pre)
+
+print("Recall metric in the testing dataset: {}%".format(100*cnf_matrix[1,1]/(cnf_matrix[1,0]+cnf_matrix[1,1])))
+#print("Precision metric in the testing dataset: {}%".format(100*cnf_matrix[0,0]/(cnf_matrix[0,0]+cnf_matrix[1,0])))
+# Plot non-normalized confusion matrix
+class_names = [0,1]
+plt.figure()
+plot_confusion_matrix(cnf_matrix , classes=class_names, title='Confusion matrix')
+plt.show()
+
+
+# In[15]:
+
+
+tmp = lr1.fit(X_train_res, y_train_res.ravel())
+
+
+# In[16]:
+
+
+y_pred_sample_score = tmp.decision_function(X_test)
+
+
+fpr, tpr, thresholds = roc_curve(y_test, y_pred_sample_score)
+
+roc_auc = auc(fpr,tpr)
+
+# Plot ROC
+plt.title('Receiver Operating Characteristic')
+plt.plot(fpr, tpr, 'b',label='AUC = %0.3f'% roc_auc)
+plt.legend(loc='lower right')
+plt.plot([0,1],[0,1],'r--')
+plt.xlim([-0.1,1.0])
+plt.ylim([-0.1,1.01])
+plt.ylabel('True Positive Rate')
+plt.xlabel('False Positive Rate')
+plt.show()
+
+
+# In[17]:
+
+
+roc_auc
+
+
+# In[18]:
+
+
+# TensorFlow and tf.keras
+import tensorflow as tf
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
+
+
+# In[19]:
+
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(256, activation='relu'),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(1)
+])
+
+
+# In[20]:
+
+
+model.compile(optimizer='adam',
+              loss='mean_absolute_error',
+              metrics=['accuracy'])
+
+
+# In[21]:
+
+
+model.fit(X_train_res,y_train_res , epochs=10)
+
+
+# In[22]:
+
+
+test_loss, test_acc = model.evaluate(X_test,  y_test, verbose=2)
+
+print('\nTest accuracy:', test_acc)
+
+
+# In[ ]:
+
+
+
+
